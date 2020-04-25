ca8d9f1f8255fe5a92340a2164390b04bf94e0ca
Morten Olsen <morten@olsen.pro>
2020-04-25 14:56:34 +0200

    Added git tree

--- zsh/.shellrc/zshrc.d/functions.zsh
+++ zsh/.shellrc/zshrc.d/functions.zsh
@@ -54,3 +54,15 @@ function short() {
   curl -F"shorten=$*" https://0x0.st
 }
 
+function git-tree {
+    # tree respecting gitignore
+
+    local ignored=$(git ls-files -ci --others --directory --exclude-standard)
+    local ignored_filter=$(echo "$ignored" \
+                    | egrep -v "^#.*$|^[[:space:]]*$" \
+                    | sed 's~^/~~' \
+                    | sed 's~/$~~' \
+                    | tr "\\n" "|")
+    tree --prune -I ".git|${ignored_filter: : -1}" "$@"
+}
+
