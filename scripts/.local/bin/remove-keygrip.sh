45766aa7a0f1e048dab35845f95d092e10ba9d37
Morten Olsen <morten@olsen.pro>
2020-07-02 22:06:39 +0200

    add remove key grip script

---
+++ scripts/.local/bin/remove-keygrip.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#gpg-connect-agent "scd serialno" "learn --force" /bye
+test ! "$@" && echo "Specify a key." && exit 1
+KEYGRIPS="$(gpg --with-keygrip --list-secret-keys $@ | grep Keygrip | awk '{print $3}')"
+for keygrip in $KEYGRIPS
+do
+    rm "$HOME/.gnupg/private-keys-v1.d/$keygrip.key" 2> /dev/null
+done
+
+gpg --card-status
