b0b46b22d09e1d9c8793d1d875788779ba151da1
Morten Olsen <morten@olsen.pro>
2020-07-02 21:49:51 +0200

    Wallpaper update

--- i3/.config/i3/compose.sh
+++ i3/.config/i3/compose.sh
@@ -1,2 +1,5 @@
 killall picom
 picom
+/usr/bin/feh --bg-fill ~/wallpaper.jpg
+wal -c
+wal -i ~/wallpaper.jpg -a 190
--- i3/.config/i3/lock.sh
+++ i3/.config/i3/lock.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
-xset s off dpms 0 10 0
-i3lock --color=000000 --ignore-empty-password --show-failed-attempts --nofork
-xset s off -dpms
+#xset s off dpms 0 10 0
+(
+  shotgun /tmp/screen.png
+)
+i3lock --color=000000 --ignore-empty-password --show-failed-attempts --nofork -i /tmp/screen.png
+#xset s off -dpms
 feh --bg-fill ~/wallpaper.jpg
